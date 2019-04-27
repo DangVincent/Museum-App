611a5d189075d840dd56157dbaf40ee4987238c1
Vincent Dang <vincentdang12345_@hotmail.com>
2019-04-27 14:28:28 -0400

    '��'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header';
+import Footer from './components/Footer';
+import SearchForm from './components/SearchForm';
+import DisplayArea from './components/DisplayArea';
+
+// Our app should have state for the current search term and results, so it can pass these things to the display area
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state  = {
+      currentSearch: '',
+      art: []
+    };
+  }
+
+  updateCurrentSearch = (currentSearch) => {
+    this.setState({
+      currentSearch
+    })
+  }
+
+  updateArt = (art) => {
+    this.setState({
+      art
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <main>
+          <SearchForm
+            updateCurrentSearch={this.updateCurrentSearch}
+            updateArt={this.updateArt}
+          />
+          <DisplayArea
+            art={this.state.art}
+            currentSearch={this.state.currentSearch}
+          />
+        </main>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
