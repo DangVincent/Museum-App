fef480654d4003e1dfe6e87e4d8ed22bae186707
Vincent Dang <vincentdang12345_@hotmail.com>
2019-04-27 14:42:10 -0400

    Updates

---
+++ precache-manifest.c5c186545933aa5b4fad84ff87b0c6a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23537280996bed76e14aeb79326364e",
+    "url": "/Museum-App/index.html"
+  },
+  {
+    "revision": "e0274684efb73a94a8c0",
+    "url": "/Museum-App/static/css/main.4747ac68.chunk.css"
+  },
+  {
+    "revision": "496d96f4d48f7360a369",
+    "url": "/Museum-App/static/js/2.50f5a3e5.chunk.js"
+  },
+  {
+    "revision": "e0274684efb73a94a8c0",
+    "url": "/Museum-App/static/js/main.f77f200a.chunk.js"
+  },
+  {
+    "revision": "27bd1aaadee52e6bec0e",
+    "url": "/Museum-App/static/js/runtime~main.96732149.js"
+  }
+]);
